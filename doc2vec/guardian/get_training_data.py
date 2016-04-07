9ac8e55bab70135e835fde365f3eca439d8cc37e
Jamis Johnson <jamismanwaring@gmail.com>
2016-04-07 20:00:00 +0000

    adds cmdline args to guardian->data script and now uses articlesv2

--- doc2vec/guardian/get_training_data.py
+++ doc2vec/guardian/get_training_data.py
@@ -1,20 +1,38 @@
-from pymongo import MongoClient
-import os
-import logging
+# python libs
+import os, logging, sys, argparse
 from datetime import datetime
-import sys
+
+# 3rd party
+from pymongo import MongoClient
 
 if __name__ == '__main__':
     # setup custom logging
     logfile = 'logs/get-training-data-{}.log'.format(datetime.now())
     logging.basicConfig(filename=logfile, level=logging.WARNING)
 
+    # parse cmd line arguments
+    parser = argparse.ArgumentParser(description='Creates doc2vec training data from guardian db')
+    parser.add_argument('-o','--output', default="articles", help='file path to output data file')
+    parser.add_argument('-s','--sections', nargs='+', type=str, default=[], help='list of sectionsNames for which to search')
+    arg = parser.parse_args()
+
     # connect to mongoDB, get articles collection
     client = MongoClient('mongodb://localhost')
-    articles = client.guardian.articles
+    articles = client.guardian.articlesv2
 
-    with open('articles_raw', 'w') as outfile:
-        docs = articles.find({'type':'article'})
+    # open outfile and begin writing articles
+    out = "data/{output}-sections-{sections}".format(
+        output = arg.output,
+        sections = "-".join(arg.sections) if arg.sections else 'all'
+    )
+    with open(out, 'w') as outfile:
+
+        # construct query from cmd line arguments
+        query = {'type':'article'}
+        if arg.sections: query.update({'sectionName': {'$in': arg.sections }})
+        docs = articles.find(query)
+
+        # iterate through documents and write text to out file
         for i, doc in enumerate(docs):
             try:
                 # get article text
