cc83d9416bb5b0bfc7bfad4e1c7cb1a9881b129e
Jamis Johnson <jamismanwaring@gmail.com>
2016-04-19 18:46:43 -0400

    adds script for scaling data.json to fit data-projector app

---
+++ scale.py
@@ -0,0 +1,22 @@
+'''scales data.json to fit data-projector'''
+
+import json, itertools
+
+# import data points
+with open('data.json','r') as fp: data = json.load(fp)
+
+# find the furthest deviance from the origin along the axes and scale using
+points = [[i['x'],i['y'],i['z']] for i in data['points']]
+maxabs = max(map(abs,itertools.chain(*points)))
+scale = 100 / maxabs
+
+# scale data
+data = { 'points': [{
+    'x': scale * point['x'],
+    'y': scale * point['y'],
+    'z': scale * point['z'],
+    'cid': i['cid']
+    } for point in data['points']] }
+
+# output to json
+with open('data.json','w') as fp: json.dump(data,fp)
