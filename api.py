6f72d148f3335d0c9e70ac00166374e44a1f1a18
Jamis Johnson <jamismanwaring@gmail.com>
2016-07-11 13:35:25 -0400

    finishes url/most_similar route. gets document text from Guardian api and other changes

--- api.py
+++ api.py
@@ -1,5 +1,10 @@
 # simple server that talks to a mongo backend
 
+# python
+from urlparse import urlparse
+from urllib import urlencode
+import json
+
 # mongo
 from pymongo import MongoClient
 client = MongoClient('mongodb://52.5.180.63') # elastic ip of mongo-dev
@@ -12,8 +17,13 @@ from flask.ext.cors import CORS, cross_origin
 
 # doc2vec
 from gensim.models import Doc2Vec
+from gensim.models.doc2vec import TaggedDocument
 # TODO load all models into lookup dict, change front-end to see many t-sne data.json
 d2v = Doc2Vec.load('ml/doc2vec/models/articles-sections-Science/dm1-mincount5-window8-size100-sample1e-05-neg0.d2v')
+init_rng_state = d2v.random.get_state()
+
+# 3rd party
+import requests
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = "Content-Type"
@@ -38,24 +48,17 @@ def get_doc(docid):
     except Exception as err:
         pass
 
-def most_similar_by_text(text):
-    # query doc2vec model to find (ids,similarities) of the most similar documents
-    similar_docids, similarities = zip(*d2v.docvecs.most_similar(docid))
-    return similar_docids, similarities
-
 def most_similar(docid_or_vector):
     '''query doc2vec model to find (ids,similarities) of the most similar documents'''
-    similar_docids, similarities = zip(*d2v.docvecs.most_similar(docid_or_vector))
+    similar_docids, similarities = zip(*d2v.docvecs.most_similar([docid_or_vector], topn=5))
     return similar_docids, similarities
 
 def get_docs_by_ids(docids):
-    # lookup documents in mongo
-    query_cursor = db.find({'_id': { '$in': [ObjectId(docid) for docid in docids] }})
-    # evaluate mongo cursor and format articles
-    docs = [format_article(a) for a in query_cursor]
-    # re-order docs list to match order of similar_docids and similarity lists
-    order = [similar_docids.index(a['id']) for a in docs]
-    docs = [article for _, article in sorted(zip(order,docs))]
+    # retrieve documents from mongo and format
+    docs = []
+    for _id in docids:
+        doc = db.find_one({'_id': ObjectId(_id)})
+        docs.append(format_article(doc))
     return docs
 
 def merge_docs_and_similarities(docs, similarities):
@@ -75,23 +78,46 @@ def get_doc_most_similar(docid):
         pass
 
 
-@app.route("/url/<url>/most_similar", methods=['GET'])
+@app.route("/url/most_similar", methods=['POST'])
 @cross_origin(origin='localhost', headers=['Content-Type'])
-def get_url_most_similar(url):
+def get_url_most_similar():
     try:
-        # TODO query guardian api to get article text
-        # text = get_text_by_url(url)
-        # TODO preprocess text the same as happened in the training of doc2vec
-        # text = preprocess(text)
-        # infer doc2vec vector of text
+        # extract url from payload
+        content = request.get_json()
+        url = content['url']
+
+        # get text of document url using Guardian api
+        params = {
+            'api-key':'e49965fd-587d-4b44-913d-6fd32587b515',
+            'show-fields': 'all',
+            'show-blocks': 'body',
+        }
+        apiurl = "http://content.guardianapis.com{path}?{query}".format(
+            path=urlparse(url).path,
+            query = urlencode(params),
+        )
+        res = requests.get(apiurl)
+        text = json.loads(res.content)['response']['content']['blocks']['body'][0]['bodyTextSummary']
+        print json.loads(res.content)['response']['content']['webTitle']
+
+        # preprocess text in the same manner as doc2vec training
+        # TODO *import* these preprocess functions from doc2vec/doc2vec.py
+        import string; printable = set(string.printable)
+        text = filter(lambda x: x in printable, text) # strip_punct
+        text = text.lower() # lower case
+
+        # get doc2vec vector given the document text
         tagged_text = TaggedDocument(words=text.split(), tags=['N/A'])
+        # for deterministic infer_vector, setting the d2v seed is
+        # required until infer_vector seed feature released
+        d2v.random.set_state(init_rng_state)
         vec = d2v.infer_vector(tagged_text.words)
+
         # find most similar docs by vector
         similar_docids, similarities = most_similar(vec)
         similar_docs = get_docs_by_ids(similar_docids)
         similar_docs = merge_docs_and_similarities(similar_docs, similarities)
-        articles, similar_docids, similarities = most_similar_by_docid(docid)
-        return jsonify({'most_similar': articles})
+        return jsonify({'most_similar': similar_docs})
     except Exception as err:
         pass
 
